38705feaa164017ae3cf4dd3dcbdf4b8bc57011e
Abhishek507-tech <abhishek096344@gmail.com>
2026-05-09 14:26:14 +0530

    Admin dashboard

---
+++ my-R/src/components/Other/AllTask.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const AllTask = () => {
+  return (
+    <div>
+      <div className='bg-[#1C1C1C] p-5 rounded mt-5  overflow-auto  overflow-y-scroll h-65 no-scrollbar'>
+       <div className='bg-red-400  mb-2 py-3 px-4 flex justify-between rounded'>
+         <h2>Abhishek Pal </h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+       </div>
+
+       <div className='bg-pink-400 mb-2 py-3 px-4 flex justify-between rounded'>
+         <h2>Abhishek Pal </h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+       </div>
+
+       <div className='bg-yellow-400 mb-2 py-3 px-4 flex justify-between rounded'>
+         <h2>Abhishek Pal </h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+       </div>
+
+       <div className='bg-orange-400 mb-2 py-3 px-4 flex justify-between rounded'>
+         <h2>Abhishek Pal </h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+       </div>
+
+       <div className='bg-purple-400 mb-2 py-3 px-4 flex justify-between rounded'>
+         <h2>Abhishek Pal </h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+       </div>
+
+       <div className='bg-emerald-400 mb-2 py-3 px-4 flex justify-between rounded'>
+         <h2>Abhishek Pal </h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+       </div>
+      </div>
+    </div>
+  )
+}
+
+export default AllTask
